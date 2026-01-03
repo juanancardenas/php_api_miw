7f2f389693033f5aebc3830c2bc158e5115c7c74
jacardenas <juanancardenas@gmail.com>
2026-01-03 22:52:06 +0100

    added implementation CGET & GET

--- src/Controller/ApiResultsQueryController.php
+++ src/Controller/ApiResultsQueryController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Result;
+use App\Entity\User;
 use App\Utility\Utils;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\{Exception\JsonException, Request, Response};
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -28,8 +30,12 @@ class ApiResultsQueryController extends AbstractController implements ApiResults
      * Constructor de la clase que gestiona los comandos tipo Query (CGET, GET y OPTIONS) y que
      * inyecta el EntityManager para que usado en los métodos de implentación de operación HTTP
      * @param EntityManagerInterface $entityManager
+     * @param LoggerInterface $logger
      */
-    public function __construct( private readonly EntityManagerInterface $entityManager ) {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly LoggerInterface $logger
+    ) {
     }
 
     /**
@@ -44,41 +50,58 @@ class ApiResultsQueryController extends AbstractController implements ApiResults
             'sort' => "id|user|result",
             '_format' => "json|xml"
         ],
-        defaults: [ '_format' => 'json', 'sort' => 'id' ],
+        defaults: [ 'sort' => 'id', '_format' => 'json', ],
         methods: [ Request::METHOD_GET, Request::METHOD_HEAD ],
     )]
     public function cgetAction(Request $request): Response
     {
         $format = Utils::getFormat($request);
-        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return Utils::errorMessage( // 401: No Authorized
+
+        // Obtenemos usuario logado. Null si no está logado
+        $loggedUser = $this->assertAuthenticated('CGET');
+        if (!$loggedUser instanceof User) {
+            // 401: No Authorized
+            return Utils::errorMessage(
                 Response::HTTP_UNAUTHORIZED,
                 'UNAUTHORIZED: Invalid credentials.',
                 $format
             );
         }
 
+        // Ordenar por parámetro seleccinado
         $order = strval($request->attributes->get('sort'));
+
+        // Si el usuario es Admin tiene acceso a todos los resultados, si no, sólo a los propios
+        $criteria = $this->isGranted('ROLE_ADMIN')
+            ? []
+            : ['user' => $loggedUser];
+
+        // Se hace la consulta
         $results = $this->entityManager
             ->getRepository(Result::class)
-            ->findBy([], [ $order => 'ASC' ]);
+            ->findBy(
+                $criteria,
+                [$order => 'ASC']
+            );
 
-        // No hay resultados
+        // No hay resultados (404: Not found)
         if (empty($results)) {
-            return Utils::errorMessage(Response::HTTP_NOT_FOUND, null, $format); // 404: Not found
+            return Utils::errorMessage(Response::HTTP_NOT_FOUND, null, $format);
         }
 
-        // Caching with ETag
+        // Caching with ETag (304: Not Modified)
         $etag = md5((string) json_encode($results, JSON_THROW_ON_ERROR));
         if (($etags = $request->getETags()) && (in_array($etag, $etags) || in_array('*', $etags))) {
-            return new Response()->setNotModified(); // 304: Not Modified
+            return new Response()->setNotModified();
         }
 
         return Utils::apiResponse(
             Response::HTTP_OK,
             ($request->isMethod(Request::METHOD_GET))
-                ? [ 'results' => array_map(fn ($res) =>  ['result' => $res], $results) ]  // GET method
-                : null, // HEAD method
+                ? [ 'results' => array_map(
+                      fn ($res) =>  ['result' => $res], $results
+                     )]  // GET method
+                : null,  // HEAD method
             $format,
             [
                 self::HEADER_CACHE_CONTROL => 'private',
@@ -105,28 +128,36 @@ class ApiResultsQueryController extends AbstractController implements ApiResults
     public function getAction(Request $request, int $resultId): Response
     {
         $format = Utils::getFormat($request);
-        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return Utils::errorMessage( // 401: Unauthorized
+
+        // Obtenemos usuario logado. Null si no está logado
+        $loggedUser = $this->assertAuthenticated('GET');
+        if (!$loggedUser instanceof User) {
+            // 401: No Authorized
+            return Utils::errorMessage(
                 Response::HTTP_UNAUTHORIZED,
                 'UNAUTHORIZED: Invalid credentials.',
                 $format
             );
         }
 
-        /** @var Result $result */
+        $criteria = $this->isGranted('ROLE_ADMIN')
+            ? ['id' => $resultId]
+            : ['id' => $resultId, 'user' => $loggedUser];
+
+        // Se hace la consulta
         $result = $this->entityManager
             ->getRepository(Result::class)
-            ->find($resultId);
+            ->findOneBy($criteria);
 
-        // No hay resultado
+        // No hay resultado (404: Not found)
         if (!$result instanceof Result) {
-            return Utils::errorMessage(Response::HTTP_NOT_FOUND, null, $format);    // 404: Not found
+            return Utils::errorMessage(Response::HTTP_NOT_FOUND, null, $format);
         }
 
-        // Caching with ETag
+        // Caching with ETag (304: Not Modified)
         $etag = md5(json_encode($result, JSON_THROW_ON_ERROR));
         if (($etags = $request->getETags()) && (in_array($etag, $etags) || in_array('*', $etags))) {
-            return (new Response())->setNotModified(); // 304: Not Modified
+            return new Response()->setNotModified();
         }
 
         return Utils::apiResponse(
@@ -169,8 +200,29 @@ class ApiResultsQueryController extends AbstractController implements ApiResults
             Response::HTTP_NO_CONTENT,
             [
                 self::HEADER_ALLOW => implode(',', $methods),
-                self::HEADER_CACHE_CONTROL => 'public, inmutable'
+                self::HEADER_CACHE_CONTROL => 'public, immutable'
             ]
         );
+    }
+
+    /**
+     * Chequea si el usuario está autenticado y saca log
+     */
+    private function assertAuthenticated(string $action): ?User
+    {
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return null;
+        }
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $this->logger->info('Authenticated user', [
+            'action' => $action,
+            'user_identifier' => $user->getUserIdentifier(),
+            'roles' => $user->getRoles()
+        ]);
+
+        return $user;
     }
 }
